b806c0b591f72e0f8c330b8f072e82d6e0ac09b2
wangshouman <wangshouman@lvmama.com>
2019-12-03 20:09:31 +0800

    增加了登录页面

--- info/static/js/index.js
+++ info/static/js/index.js
@@ -1,20 +1,41 @@
-// $(function () {
-//         $.ajax({
-//         url: "/category",
-//         type: "GET",
-//         contentType: "application/json",
-//         success: function (resp) {
-//             console.log(resp)
-//         },
-//         fail: function (error) {
-//             console.log(error)
-//         }
-//     })
-// })
+var isDialog = false
 
 function btnCategory(index, that) {
     //获取当前的分类ID
     $(that).parent('div').addClass("active");
     $(that).parent('div').siblings().removeClass("active")
     console.log(index)
+}
+
+function btnLogin() {
+    // 点击登陆按钮，弹出登录框
+
+
+    const username = $("#username").val() || '';
+    const password = $("#password").val() || '';
+    console.log("3333")
+    console.log($("#username").val())
+    console.log($("#password").val())
+    // 判断是否为空 或者校验 输入框的规则是否满足 TODO
+    if (username == '' || password == '') {
+        console.log("55555")
+    }
+    const params = {
+        "username": username,
+        "password": password
+    }
+    // 请求接口，完成登录的逻辑
+    $.ajax({
+        url: "/login",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(params),
+        success: function (resp) {
+            window.location.href="/"
+        },
+        fail: function (error) {
+            console.log(error)
+        }
+    })
+
 }
